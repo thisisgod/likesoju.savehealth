ef1c67fa424f607dfc288f077b2af77cf0e80385
armenpo <armenpo@icloud.com>
2019-10-11 23:52:40 +0900

    201910112352/모르겠다

--- code/Action1.js
+++ code/Action1.js
@@ -1,52 +1,6 @@
-module.exports.function = function action4(bodyName, startIdx, request) {
-  const console = require('console');
-  const config = require('config');
-  const http = require('http');
-  let options = {
-    format: 'json',
-    query: {
-      bodyName: bodyName
-    }
-  };
-  let response = http.getUrl(config.get('remote.url') + '/food', options);
-
-  let data = response;
-  let index = startIdx - 1;
-  let returnAction4 = new Array();
-  let i, j = 0, k = 0;
-
-  for (i = 0; i < data.length; i++) {
-    let returnObj = new Object();
-    let recipeArr = new Array();
-    returnObj.id = j + startIdx;
-    returnObj.bodyName = data[i].bodyName;
-    returnObj.foodDescription = data[i].foodDescription;
-    returnObj.foodName = data[i].foodName;
-    returnObj.mainImage = data[i].mainImage;
-    let recipeOptions = {
-    format: 'json',
-    query: {
-        foodName: returnObj.foodName
-      }
-    };
-    let recipeResponse = http.getUrl(config.get('remote.url') + '/recipe', recipeOptions);
-    for(k=0; k<recipeResponse.length; k++){
-      let recipeObj = new Object();
-      recipeObj.recipeURL = recipeResponse[k].recipeUrl;
-      recipeObj.recipeName = recipeResponse[k].recipeName;
-      recipeObj.recipeImage = recipeResponse[k].recipeImage;
-      recipeArr.push(recipeObj);
-      delete recipeObj;
-    }
-    returnObj.recipe = recipeArr;
-    console.log(recipeArr);
-    returnAction4.push(returnObj);
-    delete returnObj;
-    delete recipeArr
-    console.log(returnAction4);
-    j++;
-    if (j == 3) break;
-  }
-  for (i = 0; i < j; i++)returnAction4[i].index = startIdx + j;
-  return returnAction4;
+module.exports.function = function action1(bodyName, startIdx, request) {
+  let returnAction1 = new Object(); 
+  const search = require('./lib/SearchAction.js');
+  returnAction1 = search.searchAction1(bodyName, startIdx);
+  return returnAction1;
 }
--- code/Action3.js
+++ code/Action3.js
@@ -1,8 +1,10 @@
-module.exports.function = function action3 (foodName, bodyName) {
+module.exports.function = function action3(foodName, bodyName) {
   const console = require('console');
   const config = require('config');
   const http = require('http');
-  // const fakeData = require('./data/fakeData.js');
+  const search = require('./lib/SearchAction.js');
+  let startIdx = 1;
+
   let foodOptions = {
     format: 'json',
     query: {
@@ -11,12 +13,12 @@ module.exports.function = function action3 (foodName, bodyName) {
   };
   let response = http.getUrl(config.get('remote.url') + '/food', foodOptions);
   let data = response;
-  let i,checkIdx;
+  let i, checkIdx;
   let infoJudge = new Boolean();
   let returnAction3 = new Object();
   let isFind = false;
-  for(i=0; i<data.length; i++){
-    if(data[i].bodyName == String(bodyName)){
+  for (i = 0; i < data.length; i++) {
+    if (data[i].bodyName == String(bodyName)) {
       isFind = true;
       checkIdx = i;
     }
@@ -26,22 +28,21 @@ module.exports.function = function action3 (foodName, bodyName) {
   returnAction3.bodyName = bodyName;
   returnAction3.isFind = isFind;
 
-  if(isFind === true){
+  if (isFind === true) {
     let recipeOptions = {
-    format: 'json',
-    query: {
-      foodName: foodName
+      format: 'json',
+      query: {
+        foodName: foodName
       }
     };
     let recipeResponse = http.getUrl(config.get('remote.url') + '/recipe', recipeOptions);
     let recipeArr = new Array();
     let j;
-    for(j=0; j<recipeResponse.length; j++){
+    for (j = 0; j < recipeResponse.length; j++) {
       let recipeObj = new Object();
       recipeObj.recipeURL = recipeResponse[j].recipeUrl;
       recipeObj.recipeName = recipeResponse[j].recipeName;
       recipeObj.recipeImage = recipeResponse[j].recipeImage;
-      console.log(recipeObj);
       recipeArr.push(recipeObj);
       delete recipeObj;
     }
@@ -49,31 +50,43 @@ module.exports.function = function action3 (foodName, bodyName) {
     returnAction3.answer = foodName + "는 " + bodyName + "에 좋아요.";
     returnAction3.foodDescription = String(data[checkIdx].foodDescription);
     returnAction3.recipe = recipeArr;
+    //console.log(returnAction3);
     return returnAction3;
-  }else{
+  } else {
     let harmfulOptions = {
-    format: 'json',
-    query: {
-      foodName: foodName,
-      bodyName: bodyName
+      format: 'json',
+      query: {
+        foodName: foodName,
+        bodyName: bodyName
       }
     };
     let harmfulResponse = http.getUrl(config.get('remote.url') + '/harmful', harmfulOptions);
-    if(harmfulResponse.length < 1){
+    //console.log(harmfulResponse);
+    if (harmfulResponse.length < 1) {
       console.log("관련정보 찾지못함");
       infoJudge = false;
+      let returnAction1 = new Object();
+      let returnAction4 = new Object();
       returnAction3.infoJudge = infoJudge;
+      console.log(search.searchAction1(bodyName, startIdx));
+      returnAction1 = search.searchAction1(bodyName, startIdx);
+      returnAction3.returnAction1 = returnAction1;
       // 문구 수정필요
+      // 1. 블루베리가 좋은 신체부위
+      // 2. 블루베리가 안좋은 신체부위
+      // 3. 눈에 좋은 음식
+      // 4. 눈에 안좋은 음식
       returnAction3.answer = "관련 정보를 찾지 못했어요.";
+      //console.log(returnAction3);
       return returnAction3;
-    }else{
+    } else {
       infoJudge = true;
       returnAction3.infoJudge = infoJudge;
       // 문구 수정필요
       returnAction3.answer = foodName + "은 " + bodyName + "에 좋지않아요";
       returnAction3.foodDescription = String(harmfulResponse[0].description);
       return returnAction3;
-      
+
     }
   }
 }
